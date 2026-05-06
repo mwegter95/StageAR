7fdb5a3562b1c7ab000f24aa4b5a12497d08ff97
mwegter95 <mwegter95@gmail.com>
2026-05-06 18:04:47 -0500

    Initial Commit

---
+++ StageAR/StageARApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageARApp.swift
+//  StageAR
+//
+//  Created by Michael Wegter on 5/6/26.
+//
+
+import SwiftUI
+
+@main
+struct StageARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
